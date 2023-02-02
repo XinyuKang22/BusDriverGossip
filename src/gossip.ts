2668ff14e8fc84bfd18371b2c0321ff7d88cda7a
Xinyu Kang <xinyukang22@gmail.com>
2023-02-02 18:14:17 +1100

    Fixed eslint errors

--- src/gossip.ts
+++ src/gossip.ts
@@ -1,43 +1,57 @@
+/* eslint-disable functional/no-expression-statements */
+/* eslint-disable spellcheck/spell-checker */
+/* eslint-disable functional/prefer-readonly-type */
+/* eslint-disable total-functions/no-unsafe-mutable-readonly-assignment */
+/* eslint-disable functional/no-conditional-statements */
+/* eslint-disable no-console */
+
 const totalTime = 480;
 
 // Change the inputs below:
-const inputs = [[7 ,11 ,2 ,2 ,4 ,8 ,2 ,2],
-[3 ,0 ,11, 8],
-[5, 11, 8, 10, 3 ,11],
-[5 ,9 ,2 ,5 ,0 ,3],
-[7 ,4 ,8 ,2 ,8 ,1 ,0 ,5],
-[3 ,6 ,8 ,9],
-[4 ,2 ,11 ,3 ,3]];
-console.log(gossip(inputs, [[0],[1],[2],[3],[4],[5],[6]], 0));
+const inputs: readonly (readonly number[])[] = [
+  [3, 1, 2, 3],
+  [3, 2, 3, 1],
+  [4, 2, 3, 4, 5],
+] as const;
+const totalNumberOfDrivers = inputs.length;
 
+function gossip(
+  inputs: readonly (readonly number[])[],
+  gossips: readonly (readonly number[])[],
+  count: number
+): number {
+  if (count === totalTime) {
+    return -1;
+  }
+  console.log(count);
+  const currentStops: readonly (number | undefined)[] = inputs.map(
+    (x: readonly number[]) => x[count % x.length]
+  );
 
-function gossip(inputs:Array<Array<number>>, gossips:any, count:number):any{
-    console.log("count="+count);
-    console.log(gossips);
-    if (count == totalTime) {
-        return "never";
-    }
-    let currStops = inputs.map(x => x[count%x.length]);
-    let pairs = distinctArray(currStops).map(x => indexOfAll(currStops, x));
-    for (let p of pairs) {
-        let gossipUpdate = distinctArray(p.map((x: number) => gossips[x]).flat());
-        for (let x of p) {
-            gossips[x] = gossipUpdate;
-        }
-    }
-
-    if (gossips.every((x: any[]) => x.length == gossips.length)) {
-        return count + 1;
-    } else {
-        return gossip(inputs, gossips, count + 1);
-    }
-    
+  // eslint-disable-next-line functional/prefer-immutable-types
+  const updatedGossips: readonly number[][] = gossips.map((_, driver) =>
+    removeDuplicate(
+      indexOfAll(currentStops, currentStops[driver])
+        .map((theDriver) => gossips[theDriver])
+        .flat()
+    )
+  );
+  console.log(updatedGossips);
+  const isGossipComplete = updatedGossips.every(
+    (x: readonly (number | undefined)[]) => x.length === totalNumberOfDrivers
+  );
+  return isGossipComplete
+    ? count + 1
+    : gossip(inputs, updatedGossips, count + 1);
 }
 
-function indexOfAll (arr: any[], val: any) {
-    return arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), []);
+function indexOfAll(arr: readonly (number | undefined)[], val?: number) {
+  const empty: readonly number[] = [];
+  return arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), empty);
 }
 
-function distinctArray (arrayWithDuplicates:any[]) {
-    return arrayWithDuplicates.filter((n, i) => arrayWithDuplicates.indexOf(n) === i);
-}
+const removeDuplicate = <T>(arrayWithDuplicates: readonly T[]): readonly T[] =>
+  arrayWithDuplicates.filter((n, i) => arrayWithDuplicates.indexOf(n) === i);
+
+const result = gossip(inputs, [[0], [1], [2]], 0);
+console.log(result === -1 ? "never" : result);

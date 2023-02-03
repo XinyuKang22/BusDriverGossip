002618e7d81823429e9cdf10b72b4b88938a95f4
Xinyu Kang <xinyukang22@gmail.com>
2023-02-03 15:55:53 +1100

    IE-84 = bus-gossip-kata: Use BDD style in unit tests

--- src/gossip.test.ts
+++ src/gossip.test.ts
@@ -6,29 +6,42 @@
 
 import { gossip } from "./gossip";
 
-describe("Testing", () => {
-  it.each([
-    {
-      inputs: [
-        [3, 1, 2, 3],
-        [3, 2, 3, 1],
-        [4, 2, 3, 4, 5],
-      ],
-      expected: 5,
-    },
-    {
-      inputs: [
-        [2, 1, 2],
-        [5, 2, 8],
-      ],
-      expected: "never",
-    },
-    {
-      inputs: [[2], [2], [2]],
-      expected: 1,
-    },
-    // eslint-disable-next-line functional/prefer-immutable-types
-  ])("Test", ({ inputs, expected }) => {
-    expect(gossip(inputs)).toBe(expected);
+describe("Given there are at least two routes(drivers)", () => {
+  describe("When one or more drivers do not have all the gossips by the end of the day", () => {
+    it.each([
+      {
+        inputs: [
+          [2, 1, 2],
+          [5, 2, 8],
+        ],
+        expected: "never",
+      },
+      {
+        inputs: [[1], [2], [0]],
+        expected: "never",
+      },
+      // eslint-disable-next-line functional/prefer-immutable-types
+    ])("should return 'never'", ({ inputs, expected }) => {
+      expect(gossip(inputs)).toBe(expected);
+    });
+  });
+  describe("When all drivers have all the gossips by the end of the day", () => {
+    it.each([
+      {
+        inputs: [
+          [3, 1, 2, 3],
+          [3, 2, 3, 1],
+          [4, 2, 3, 4, 5],
+        ],
+        expected: 5,
+      },
+      {
+        inputs: [[2], [2], [2]],
+        expected: 1,
+      },
+      // eslint-disable-next-line functional/prefer-immutable-types
+    ])("should return the time it takes", ({ inputs, expected }) => {
+      expect(gossip(inputs)).toBe(expected);
+    });
   });
 });

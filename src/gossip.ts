8f7a909f282744de963fd5ea5c440d03a00d458e
Xinyu Kang <xinyukang22@gmail.com>
2023-02-03 14:20:19 +1100

    IE-84 = bus-gossip-kata: Add comments

--- src/gossip.ts
+++ src/gossip.ts
@@ -1,15 +1,20 @@
-/* eslint-disable prettier/prettier */
 /* eslint-disable spellcheck/spell-checker */
 /* eslint-disable total-functions/no-unsafe-mutable-readonly-assignment */
 
-export function gossip (inputs: readonly (readonly number[])[]) :number | string {
-  // eslint-disable-next-line functional/prefer-immutable-types
-  const initialGossips: readonly (readonly number[])[] = Array.from(Array(inputs.length).keys()).map(i => [i]);
-  const result = gossip_(inputs, initialGossips, 0);
+/**
+ * Find the number of stops it takes to have all drivers on board with the latest gossips.
+ * If there is even one driver who does not have all the gossips by the end of the day, return 'never'.
+ * @param inputs driver routes
+ * @returns
+ */
+export function gossip(
+  inputs: readonly (readonly number[])[]
+): number | string {
+  const result = gossip_(inputs, initialGossips(inputs.length), 0);
   return result === -1 ? "never" : result;
 }
 
-const totalTime = 480;
+const totalTimeMinutes = 480;
 
 function gossip_(
   inputs: readonly (readonly number[])[],
@@ -17,20 +22,20 @@ function gossip_(
   count: number
 ): number {
   // eslint-disable-next-line functional/no-conditional-statements
-  if (count === totalTime) {
+  if (count === totalTimeMinutes) {
     return -1;
   }
-  const currentStops: readonly (number | undefined)[] = inputs.map(
-    (x: readonly number[]) => x[count % x.length]
-  );
-  const updatedGossips: readonly (readonly (number | undefined)[])[] = gossips.map(
-    (_, driver) =>
+  const currentStops: readonly (number | undefined)[] = inputs
+    .map((x: readonly number[]) => x[count % x.length])
+    .filter((x?: number): x is number => x !== undefined);
+  const updatedGossips: readonly (readonly (number | undefined)[])[] =
+    gossips.map((_, driver) =>
       removeDuplicate(
         indexOfAll(currentStops, currentStops[driver])
           .map((theDriver) => gossips[theDriver])
           .flat()
       )
-  );
+    );
   const isGossipComplete = updatedGossips.every(
     (x: readonly (number | undefined)[]) => x.length === inputs.length
   );
@@ -39,6 +44,12 @@ function gossip_(
     : gossip_(inputs, updatedGossips, count + 1);
 }
 
+/**
+ * Find all occurences of val in arr and return indexes
+ * @param arr
+ * @param val
+ * @returns
+ */
 function indexOfAll(arr: readonly (number | undefined)[], val?: number) {
   const empty: readonly number[] = [];
   return arr.reduce((acc, el, i) => (el === val ? [...acc, i] : acc), empty);
@@ -47,3 +58,16 @@ function indexOfAll(arr: readonly (number | undefined)[], val?: number) {
 const removeDuplicate = <T>(arrayWithDuplicates: readonly T[]): readonly T[] =>
   arrayWithDuplicates.filter((n, i) => arrayWithDuplicates.indexOf(n) === i);
 
+/**
+ * Generate an initial gossip array for drivers.
+ * (
+ * For example,
+ * if totalDriver = 2, then initialGossips = [[0], [1]]
+ * if totalDriver = 4, then initialGossips = [[0], [1], [2], [3]]
+ * )
+ * @param totalDriver total number of drivers/routes from inputs
+ * @returns
+ */
+const initialGossips = (totalDriver: number): readonly (readonly number[])[] =>
+  // eslint-disable-next-line functional/prefer-immutable-types
+  Array.from(Array(totalDriver).keys()).map((i) => [i]);
